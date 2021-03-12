0244fe96601b3501451cd9c7ebce3dab0ac19a06
Sachin <sachin.madan@jungleworks.com>
2021-03-12 20:42:10 +0530

    Use of UIDynamicAnimator

--- Wysa Animator/ViewController.swift
+++ Wysa Animator/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var viewToAnimate: UIView!
+    private var animator: UIDynamicAnimator!
+    private var snapping: UISnapBehavior!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        animator = UIDynamicAnimator(referenceView: view)
+        snapping = UISnapBehavior(item: viewToAnimate, snapTo: view.center)
+        animator.addBehavior(snapping)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pannedView))
+        viewToAnimate.addGestureRecognizer(panGesture)
+        viewToAnimate.isUserInteractionEnabled = true
     }
 
-
+    @objc func pannedView(recognizer: UIPanGestureRecognizer){
+        switch recognizer.state{
+        
+        case .began:
+            animator.removeBehavior(snapping)
+        case .changed:
+            let translation = recognizer.translation(in: view)
+            viewToAnimate.center = CGPoint(x: viewToAnimate.center.x + translation.x, y: viewToAnimate.center.y + translation.y)
+            recognizer.setTranslation(.zero, in: view)
+        case .ended , .failed , .cancelled:
+            animator.addBehavior(snapping)
+        case .possible:
+            break
+        @unknown default:
+            break
+        }
+        
+    }
 }
 
